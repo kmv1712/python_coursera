1c4fbfee2d594e11dee3bbcae2d227943137e282
mv.karpov <mv.karpov@tensor.ru>
2019-01-31 16:26:33 +0500

    Задание с машинами

---
+++ Курс Погружение в python/car.py
@@ -0,0 +1,31 @@
+class CarBase:
+    def __init__(self, brand, photo_file_name, carrying):
+        self.brand = brand
+        self.photo_file_name = photo_file_name
+        self.carrying = carrying
+
+    def get_photo_file_ext(self):
+    	file_ext = os.path.splitext(self.photo_file_name)
+    	file_ext = file_ext[1]
+    	return file_ext
+    pass
+
+
+class Car(CarBase):
+    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
+        pass
+
+
+class Truck(CarBase):
+    def __init__(self, brand, photo_file_name, carrying, body_whl):
+        pass
+
+
+class SpecMachine(CarBase):
+    def __init__(self, brand, photo_file_name, carrying, extra):
+        pass
+
+
+def get_car_list(csv_filename):
+    car_list = []
+    return car_list
